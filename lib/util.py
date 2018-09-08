3f9aa2230b2a82d64c3cf73bb817d4a5fca7147f
JayChen <marsyeti@gmail.com>
2018-09-08 22:33:05 +0800

    changed to tushare pro api for more stable connection

--- lib/util.py
+++ lib/util.py
@@ -1,26 +1,14 @@
 import datetime
-
+import time
 import pandas as pd
 import tushare as ts
 
 from setting import split_span
+from lib.token import TOKEN
 
-
-def six_digit_date_split(six_digit):
-    """
-    utility func to convert six digit date in stock_list.csv into yyyy-mm-dd format string
-    Parameters
-    ----------
-    six_digit: int
-        date to be converted
-
-    Returns
-    -------
-    yyyy-mm-dd format string
-    """
-    six_digit = str(six_digit)
-    six_digit = six_digit[:4] + "-" + six_digit[4:6] + "-" + six_digit[6:]
-    return six_digit
+# setting pro api, please use your own token
+ts.set_token(TOKEN)
+PRO = ts.pro_api()
 
 
 def date_split(start_date, end_date, span=split_span):
@@ -30,28 +18,28 @@ def date_split(start_date, end_date, span=split_span):
     Parameters
     ----------
     start_date: str
-        start date with format yyyy-mm-dd
+        start date with format yyyymmdd
     end_date: str
-        end date with format yyyy-mm-dd
+        end date with format yyyymmdd
     span:int
         days of time span, must > 1 or there will be bug
 
     Returns
     -------
-    list of start/end date tuples ("yyyy-mm-dd", "yyyy-mm-dd")
+    list of start/end date tuples ("yyyymmdd", "yyyymmdd")
     """
-    start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
-    end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+    start_date = datetime.datetime.strptime(start_date, "%Y%m%d")
+    end_date = datetime.datetime.strptime(end_date, "%Y%m%d")
     time_spans = []
 
     while (end_date - start_date).days > span:
-        s = datetime.datetime.strftime(start_date, "%Y-%m-%d")
+        s = datetime.datetime.strftime(start_date, "%Y%m%d")
         start_date = start_date + datetime.timedelta(span)
-        e = datetime.datetime.strftime(start_date - datetime.timedelta(1), "%Y-%m-%d")
+        e = datetime.datetime.strftime(start_date - datetime.timedelta(1), "%Y%m%d")
         time_spans.append((s, e))
 
-    s = datetime.datetime.strftime(start_date, "%Y-%m-%d")
-    e = datetime.datetime.strftime(end_date, "%Y-%m-%d")
+    s = datetime.datetime.strftime(start_date, "%Y%m%d")
+    e = datetime.datetime.strftime(end_date, "%Y%m%d")
     time_spans.append((s, e))
 
     return time_spans
@@ -66,9 +54,9 @@ def hist_data_down(stock_id, start_date, end_date):
     stock_id: str
         stock id code
     start_date: str
-        start date with format yyyy-mm-dd
+        start date with format yyyymmdd
     end_date: str
-        end date with format yyyy-mm-dd
+        end date with format yyyymmdd
 
     Returns
     -------
@@ -79,7 +67,9 @@ def hist_data_down(stock_id, start_date, end_date):
 
     for start, end in reversed(time_spans):
         try:
-            dfs.append(ts.get_h_data(stock_id, start=start, end=end, pause=1))
+            d = PRO.daily(ts_code=stock_id, start_date=start, end_date=end)
+            dfs.append(d)
+            time.sleep(1)
         except:
             print("{} to {} download error".format(start, end))
 
@@ -88,6 +78,6 @@ def hist_data_down(stock_id, start_date, end_date):
     return df
 
 # if __name__ == "__main__":
-#     print(date_split("2012-01-02", "2012-01-05", 1))
-#     df = hist_data_down("600419", "2010-10-10", "2018-02-02")
-#     df.to_csv("exp.csv")
+# print(date_split("20020102", "20120105", 100))
+# df = hist_data_down("600419.SH", "20101010", "20180202")
+# df.to_csv("exp.csv", index=None)
